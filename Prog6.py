56612c2003d05ab27a74e2833fcb84deb45ba625
akshay4pec <120158466+akshay4pec@users.noreply.github.com>
2023-01-13 10:57:03 +0530

    Update Prog6.py

--- Prog6.py
+++ Prog6.py
@@ -7,7 +7,7 @@ for i in range(a,b+1):
         continue
     prime = 1
 
-    for j in range(2 , i//2 + 1):
+    for j in range(2 , i):
         if(i%j == 0):
             prime = 0
             break
